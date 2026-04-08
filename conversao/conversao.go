111fc7c94aabb70c226dd52c1da4b6895c5ce2b5
mastergss <gsosilva@mail.com>
2026-04-08 10:57:18 -0300

    conversão de tipos

---
+++ conversao/conversao.go
@@ -0,0 +1,18 @@
+// Em Go, a conversão de tipos é feita explicitamente. Para converter um int para float64, usamos a sintaxe: float64(a)
+package main
+
+import "fmt"
+// Declarando as variáveis 'a' e 'b' escopo global
+var a int = 20
+var b string = "nome"
+
+func main() {
+	fmt.Println("Valor de a:", a)
+	fmt.Println("Valor de b:", b)
+
+	// Convertendo 'var a' para float64
+	var c float64 = float64(a)
+	fmt.Println("Valor de c (float64):", c)
+
+	fmt.Printf("O valor de c é: %g e o valor de b é: %s", c, b)
+}

9a148bfc79e0bf91a09da7a74fda9bab42a86d61
mastergss <gsosilva@mail.com>
2026-04-13 12:07:35 -0300

    criar estruturas em JSON para receber os dados de "usuarios.json"

--- json/lerjson.go
+++ json/lerjson.go
@@ -7,6 +7,27 @@ import (
 	"os"
 )
 
+//Criar estruturas em JSON para receber os dados de "usuarios.json"
+
+//estrutura "Usuarios" para listar "usuarios.json"
+type Usuarios struct {
+	Usuarios []Usuario `json: "usuarios"`
+}
+
+//estrutura para receber os dados em Json que estão antes de "Social"
+type Usuario struct {
+	Nome string `json: "Nome"`
+	Tipo string `json: "Tipo"`
+	Idade int `json: "Idade"`
+	Social string `json: "Social"`
+}
+
+//estrutura para receber os dados em Json que estão em "Social"
+type Social struct {
+	Facebook string `json: "Facebook"`
+	Twitter  string `json: "Twitter"`
+}
+
 func main() {
 	//Abre o arquivo nomeado com a flag especificada
 	//Se bem-sucedido, o arquivo retornado pode ser usado para leitura

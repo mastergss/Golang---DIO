8ca82d72c687f89982bd22245c018d753eeb44ce
mastergss <gsosilva@mail.com>
2026-04-22 12:15:33 -0300

    padrão triple A - arrange, act, assert

--- testes/soma_test.go
+++ testes/soma_test.go
@@ -3,11 +3,19 @@ package main
 import "testing"
 
 // Convenção de nomes - a assinatura do método deve iniciar com Should(função)Correct/Incorrect
+/*
+Padrão Triple 'A' - AAA
+A - Arrange =	Organizar/Preparar
+A - Act		=	Ação/Rodar
+A - Assert	=	Afirmar/Verificar Asserções
+*/
 
 func ShouldSumCorrect(t *testing.T) { //func TestSoma(t *testing.T) {
+	// Arrange
 	teste := soma(3, 2, 1)
+	// Act
 	resultado := 6
-
+	// Assert
 	if teste != resultado {
 		t.Error("Valor esperado: ", resultado, "Valor retornado: ", teste)
 	}

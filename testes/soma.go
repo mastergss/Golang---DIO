25e9daa75dd006dbaed43e5cc78d1677c4633a5b
mastergss <gsosilva@mail.com>
2026-04-22 11:52:07 -0300

    convenção de nomes - a assinatura do método deve iniciar com Should(função)Correct/Incorrect

--- testes/soma.go
+++ testes/soma.go
@@ -4,13 +4,22 @@ import "fmt"
 
 func main() {
 	x := soma(1, 2, 3)
-	fmt.Println(x)
+	y := multiplica (10, 10)
+	fmt.Println(x, y)
 }
 
 func soma(i ...int) int{
 	total := 0
 	for _, valor := range i {
 		total += valor
+	}
+	return total
+}
+
+func multiplica(i ...int) int{
+	total := 1
+	for _, valor := range i {
+		total *= valor
 	}
 	return total
 }
--- testes/soma_test.go
+++ testes/soma_test.go
@@ -2,8 +2,10 @@ package main
 
 import "testing"
 
-func TestSoma(t *testing.T) {
-	teste := soma(3 ,2 ,1)
+// Convenção de nomes - a assinatura do método deve iniciar com Should(função)Correct/Incorrect
+
+func ShouldSumCorrect(t *testing.T) { //func TestSoma(t *testing.T) {
+	teste := soma(3, 2, 1)
 	resultado := 6
 
 	if teste != resultado {
@@ -11,11 +13,29 @@ func TestSoma(t *testing.T) {
 	}
 }
 
-func TestSoma2(t *testing.T) {
+func ShouldSumIncorrect(t *testing.T) { //func TestSoma2(t *testing.T) {
 	teste := soma(3, 2, 1)
-	resultado :=7
+	resultado := 7
 
 	if teste != resultado {
 		t.Error("Valor esperado: ", resultado, "Valor retornado: ", teste)
+	}
+}
+
+func ShouldMultCorrect(t *testing.T) { //func TestMultiplica(t *testing.T) {
+	teste := multiplica(10, 10)
+	resultado := 100
+
+	if teste != resultado {
+		t.Error("Valor esperado: ", resultado, "Valor obtido: ", teste)
+	}
+}
+
+func ShouldMultIncorrect(t *testing.T) { //func TestMultiplica2(t *testing.T) {
+	teste := multiplica(10, 10)
+	resultado := 2560
+
+	if teste != resultado {
+		t.Error("Valor esperado: ", resultado, "Valor obtido: ", teste)
 	}
 }
